c80cafdcd428867009b38c2ce8ab57ba4e213caf
ppalex <prietopantoja.alex@gmail.com>
2020-04-04 14:14:46 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Installation file for script app.py."""
+
+from cx_Freeze import setup, Executable
+
+
+setup(
+    name = "application",
+    version = "0.1",
+    description = "Projet mac_gyver",
+    executables = [Executable("app.py")],
+)
